5d5f034dd603636fd5d41376dce7ae67b475d1f2
NickV1199 <volkovnikolai@protonmail.com>
2026-05-11 20:52:32 +0300

    added addnew car steps

--- Homrwork2/src/main/java/manager/ApplicationManager.java
+++ Homrwork2/src/main/java/manager/ApplicationManager.java
@@ -9,6 +9,7 @@ public class ApplicationManager {
 
     WebDriver wd;
     HelperUser helperUser;
+    HelperCar helperCar;
 
 
     public void init() {
@@ -17,6 +18,7 @@ public class ApplicationManager {
         wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
         wd.navigate().to("https://ilcarro.web.app/search");
         helperUser = new HelperUser(wd);
+        helperCar = new HelperCar(wd);
 
 
 
@@ -28,6 +30,10 @@ public class ApplicationManager {
 
     public HelperUser getHelperUser() {
         return helperUser;
+    }
+
+    public HelperCar getHelperCar(){
+        return helperCar;
     }
 
 }
---
+++ Homrwork2/src/main/java/manager/HelperCar.java
@@ -0,0 +1,36 @@
+package manager;
+
+import org.openqa.selenium.WebDriver;
+
+public class HelperCar extends HelperBase{
+
+    public HelperCar(WebDriver wd){
+        super(wd);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
