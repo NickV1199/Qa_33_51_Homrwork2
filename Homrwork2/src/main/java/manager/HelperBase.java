8d04582a0ccfbc5e7be0af34ae628a36d79e9e8f
NickV1199 <volkovnikolai@protonmail.com>
2026-05-27 18:20:44 +0300

    listener added

--- Homrwork2/src/main/java/manager/HelperBase.java
+++ Homrwork2/src/main/java/manager/HelperBase.java
@@ -1,11 +1,11 @@
 package manager;
 
+import com.google.common.io.Files;
 import net.bytebuddy.implementation.bytecode.Throw;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 
+import java.io.File;
+import java.io.IOException;
 import java.security.Key;
 import java.util.List;
 
@@ -66,5 +66,17 @@ public class HelperBase {
     public String getErrorText() {
         return wd.findElement(By.cssSelector("div.error")).getText();
     }
+
+    public void getScreen(String link) {
+        TakesScreenshot takesScreenshot = (TakesScreenshot) wd;
+        File tmp = takesScreenshot.getScreenshotAs(OutputType.FILE);
+        try {
+            Files.copy(tmp, new File(link));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
 
 }
--- Homrwork2/src/test/java/tests/TestBase.java
+++ Homrwork2/src/test/java/tests/TestBase.java
@@ -3,6 +3,7 @@ package tests;
 import manager.ApplicationManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
@@ -26,6 +27,10 @@ public class TestBase {
         logger.info("Name of method(test) -->"+m.getName());
     }
 
+    @AfterMethod
+    public void end(){
+        logger.info("=============================================================================");
+    }
 
     @AfterSuite
 
